9ed46cd14b0d4a67f7426d1c625f2f337c0bc111
clickandgo-informatique <mail@clickandgo-informatique.fr>
2026-05-28 14:59:52 +0200

    Fix Nginx auto-HTTPS: disable default/ssl conf, entrypoint logic

--- docker/nginx/prod/entrypoint.sh
+++ docker/nginx/prod/entrypoint.sh
@@ -4,20 +4,13 @@ echo ">>> Démarrage de Nginx..."
 SSL_CERT="/etc/letsencrypt/live/app.m-motors.clickandgo-informatique.com/fullchain.pem"
 SSL_KEY="/etc/letsencrypt/live/app.m-motors.clickandgo-informatique.com/privkey.pem"
 
-# S'assurer que le dossier existe
-mkdir -p /etc/nginx/conf.d
+rm -f /etc/nginx/conf.d/*.conf
 
-# Nettoyer les anciennes conf actives
-rm -f /etc/nginx/conf.d/active.conf
-rm -f /etc/nginx/conf.d/active-ssl.conf
+cp /etc/nginx/conf.d/default.conf.disabled /etc/nginx/conf.d/active.conf
 
-# Toujours charger la conf HTTP
-cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/active.conf
-
-# Si le certificat existe, activer HTTPS
 if [ -f "$SSL_CERT" ] && [ -f "$SSL_KEY" ]; then
   echo ">>> Certificat trouvé, activation de HTTPS"
-  cp /etc/nginx/conf.d/ssl.conf /etc/nginx/conf.d/active-ssl.conf
+  cp /etc/nginx/conf.d/ssl.conf.disabled /etc/nginx/conf.d/active-ssl.conf
 else
   echo ">>> Aucun certificat trouvé, démarrage en HTTP uniquement"
 fi
--- migrations/Version20260528124117.php
+++ migrations/Version20260528124117.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260527224545 extends AbstractMigration
+final class Version20260528124117 extends AbstractMigration
 {
     public function getDescription(): string
     {
