33c3453fe6d2b7d1860d9e5d72d7707cd813bde0
clickandgo-informatique <mail@clickandgo-informatique.fr>
2026-05-28 12:39:10 +0200

    Mise à jour config Docker + Nginx + Certbot

--- docker/nginx/prod/entrypoint.sh
+++ docker/nginx/prod/entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 set -e
 
-DOMAIN="app.m-motors.clickandgo-informatique.com"
-CERT_PATH="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
-
-# Si les certificats n'existent pas, on lance certbot
-if [ ! -f "$CERT_PATH" ]; then
-  echo ">>> Aucun certificat trouvé, lancement de Certbot..."
-  certbot certonly --webroot \
-    --webroot-path=/var/www/certbot \
-    -d $DOMAIN \
-    --email ton-email@example.com \
-    --agree-tos \
-    --non-interactive
-fi
-
-echo ">>> Certificats OK, démarrage de Nginx..."
+echo ">>> Démarrage de Nginx..."
 exec nginx -g "daemon off;"
+
